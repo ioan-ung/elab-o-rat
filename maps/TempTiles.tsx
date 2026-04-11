afb57bbf3634933f65aa3dfc3ab313847e92444c
Nistor Constantin <constantin.nistor@student.tuiasi.ro>
2026-04-11 20:45:50 +0300

    Prototype Tutorial Map

--- maps/TempTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="TempTiles" tilewidth="32" tileheight="32" tilecount="51" columns="17">
- <image source="paoo-proiect-ungureanu_nistor/materials/RandomAssests/Diceforce_assets/Tiles/Tiles.png" width="544" height="96"/>
-</tileset>
